8f9a521c12f376c5540e295d81bd6261fa620ecf
Siyu <schen347@wisc.edu>
2018-04-01 06:55:27 -0400

    add amcharts

---
+++ charts/data.js
@@ -0,0 +1,554 @@
+var chartDataOrigin = [
+    {
+        "Entity": "Afghanistan",
+        "Code": "AFG",
+        "Year": "1970",
+        "Terrorism incidents (GTD) (terrorism-related incidents)": "0"
+    },
+    {
+        "Entity": "Afghanistan",
+        "Code": "AFG",
+        "Year": "1971",
+        "Terrorism incidents (GTD) (terrorism-related incidents)": "0"
+    },
+    {
+        "Entity": "Afghanistan",
+        "Code": "AFG",
+        "Year": "1972",
+        "Terrorism incidents (GTD) (terrorism-related incidents)": "0"
+    },
+    {
+        "Entity": "Afghanistan",
+        "Code": "AFG",
+        "Year": "1973",
+        "Terrorism incidents (GTD) (terrorism-related incidents)": "1"
+    },
+    {
+        "Entity": "Afghanistan",
+        "Code": "AFG",
+        "Year": "1974",
+        "Terrorism incidents (GTD) (terrorism-related incidents)": "0"
+    },
+    {
+        "Entity": "Afghanistan",
+        "Code": "AFG",
+        "Year": "1975",
+        "Terrorism incidents (GTD) (terrorism-related incidents)": "0"
+    },
+    {
+        "Entity": "Afghanistan",
+        "Code": "AFG",
+        "Year": "1976",
+        "Terrorism incidents (GTD) (terrorism-related incidents)": "0"
+    },
+    {
+        "Entity": "Afghanistan",
+        "Code": "AFG",
+        "Year": "1977",
+        "Terrorism incidents (GTD) (terrorism-related incidents)": "0"
+    },
+    {
+        "Entity": "Afghanistan",
+        "Code": "AFG",
+        "Year": "1978",
+        "Terrorism incidents (GTD) (terrorism-related incidents)": "0"
+    },
+    {
+        "Entity": "Afghanistan",
+        "Code": "AFG",
+        "Year": "1979",
+        "Terrorism incidents (GTD) (terrorism-related incidents)": "3"
+    },
+    {
+        "Entity": "Afghanistan",
+        "Code": "AFG",
+        "Year": "1980",
+        "Terrorism incidents (GTD) (terrorism-related incidents)": "0"
+    },
+    {
+        "Entity": "Afghanistan",
+        "Code": "AFG",
+        "Year": "1981",
+        "Terrorism incidents (GTD) (terrorism-related incidents)": "0"
+    },
+    {
+        "Entity": "Afghanistan",
+        "Code": "AFG",
+        "Year": "1982",
+        "Terrorism incidents (GTD) (terrorism-related incidents)": "0"
+    },
+    {
+        "Entity": "Afghanistan",
+        "Code": "AFG",
+        "Year": "1983",
+        "Terrorism incidents (GTD) (terrorism-related incidents)": "0"
+    },
+    {
+        "Entity": "Afghanistan",
+        "Code": "AFG",
+        "Year": "1984",
+        "Terrorism incidents (GTD) (terrorism-related incidents)": "0"
+    },
+    {
+        "Entity": "Afghanistan",
+        "Code": "AFG",
+        "Year": "1985",
+        "Terrorism incidents (GTD) (terrorism-related incidents)": "0"
+    },
+    {
+        "Entity": "Afghanistan",
+        "Code": "AFG",
+        "Year": "1986",
+        "Terrorism incidents (GTD) (terrorism-related incidents)": "0"
+    },
+    {
+        "Entity": "Afghanistan",
+        "Code": "AFG",
+        "Year": "1987",
+        "Terrorism incidents (GTD) (terrorism-related incidents)": "1"
+    },
+    {
+        "Entity": "Afghanistan",
+        "Code": "AFG",
+        "Year": "1988",
+        "Terrorism incidents (GTD) (terrorism-related incidents)": "11"
+    },
+    {
+        "Entity": "Afghanistan",
+        "Code": "AFG",
+        "Year": "1989",
+        "Terrorism incidents (GTD) (terrorism-related incidents)": "10"
+    },
+    {
+        "Entity": "Afghanistan",
+        "Code": "AFG",
+        "Year": "1990",
+        "Terrorism incidents (GTD) (terrorism-related incidents)": "2"
+    },
+    {
+        "Entity": "Afghanistan",
+        "Code": "AFG",
+        "Year": "1991",
+        "Terrorism incidents (GTD) (terrorism-related incidents)": "30"
+    },
+    {
+        "Entity": "Afghanistan",
+        "Code": "AFG",
+        "Year": "1992",
+        "Terrorism incidents (GTD) (terrorism-related incidents)": "36"
+    },
+    {
+        "Entity": "Afghanistan",
+        "Code": "AFG",
+        "Year": "1994",
+        "Terrorism incidents (GTD) (terrorism-related incidents)": "9"
+    },
+    {
+        "Entity": "Afghanistan",
+        "Code": "AFG",
+        "Year": "1995",
+        "Terrorism incidents (GTD) (terrorism-related incidents)": "6"
+    },
+    {
+        "Entity": "Afghanistan",
+        "Code": "AFG",
+        "Year": "1996",
+        "Terrorism incidents (GTD) (terrorism-related incidents)": "4"
+    },
+    {
+        "Entity": "Afghanistan",
+        "Code": "AFG",
+        "Year": "1997",
+        "Terrorism incidents (GTD) (terrorism-related incidents)": "1"
+    },
+    {
+        "Entity": "Afghanistan",
+        "Code": "AFG",
+        "Year": "1998",
+        "Terrorism incidents (GTD) (terrorism-related incidents)": "1"
+    },
+    {
+        "Entity": "Afghanistan",
+        "Code": "AFG",
+        "Year": "1999",
+        "Terrorism incidents (GTD) (terrorism-related incidents)": "9"
+    },
+    {
+        "Entity": "Afghanistan",
+        "Code": "AFG",
+        "Year": "2000",
+        "Terrorism incidents (GTD) (terrorism-related incidents)": "14"
+    },
+    {
+        "Entity": "Afghanistan",
+        "Code": "AFG",
+        "Year": "2001",
+        "Terrorism incidents (GTD) (terrorism-related incidents)": "14"
+    },
+    {
+        "Entity": "Afghanistan",
+        "Code": "AFG",
+        "Year": "2002",
+        "Terrorism incidents (GTD) (terrorism-related incidents)": "38"
+    },
+    {
+        "Entity": "Afghanistan",
+        "Code": "AFG",
+        "Year": "2003",
+        "Terrorism incidents (GTD) (terrorism-related incidents)": "100"
+    },
+    {
+        "Entity": "Afghanistan",
+        "Code": "AFG",
+        "Year": "2004",
+        "Terrorism incidents (GTD) (terrorism-related incidents)": "88"
+    },
+    {
+        "Entity": "Afghanistan",
+        "Code": "AFG",
+        "Year": "2005",
+        "Terrorism incidents (GTD) (terrorism-related incidents)": "155"
+    },
+    {
+        "Entity": "Afghanistan",
+        "Code": "AFG",
+        "Year": "2006",
+        "Terrorism incidents (GTD) (terrorism-related incidents)": "282"
+    },
+    {
+        "Entity": "Afghanistan",
+        "Code": "AFG",
+        "Year": "2007",
+        "Terrorism incidents (GTD) (terrorism-related incidents)": "340"
+    },
+    {
+        "Entity": "Afghanistan",
+        "Code": "AFG",
+        "Year": "2008",
+        "Terrorism incidents (GTD) (terrorism-related incidents)": "415"
+    },
+    {
+        "Entity": "Afghanistan",
+        "Code": "AFG",
+        "Year": "2009",
+        "Terrorism incidents (GTD) (terrorism-related incidents)": "502"
+    },
+    {
+        "Entity": "Afghanistan",
+        "Code": "AFG",
+        "Year": "2010",
+        "Terrorism incidents (GTD) (terrorism-related incidents)": "541"
+    },
+    {
+        "Entity": "Afghanistan",
+        "Code": "AFG",
+        "Year": "2011",
+        "Terrorism incidents (GTD) (terrorism-related incidents)": "421"
+    },
+    {
+        "Entity": "Afghanistan",
+        "Code": "AFG",
+        "Year": "2012",
+        "Terrorism incidents (GTD) (terrorism-related incidents)": "1468"
+    },
+    {
+        "Entity": "Afghanistan",
+        "Code": "AFG",
+        "Year": "2013",
+        "Terrorism incidents (GTD) (terrorism-related incidents)": "1441"
+    },
+    {
+        "Entity": "Afghanistan",
+        "Code": "AFG",
+        "Year": "2014",
+        "Terrorism incidents (GTD) (terrorism-related incidents)": "1821"
+    },
+    {
+        "Entity": "Afghanistan",
+        "Code": "AFG",
+        "Year": "2015",
+        "Terrorism incidents (GTD) (terrorism-related incidents)": "1927"
+    },
+    {
+        "Entity": "Afghanistan",
+        "Code": "AFG",
+        "Year": "2016",
+        "Terrorism incidents (GTD) (terrorism-related incidents)": "1615"
+    },
+    {
+        "Entity": "United States",
+        "Code": "USA",
+        "Year": "1970",
+        "Terrorism incidents (GTD) (terrorism-related incidents)": "468"
+    },
+    {
+        "Entity": "United States",
+        "Code": "USA",
+        "Year": "1971",
+        "Terrorism incidents (GTD) (terrorism-related incidents)": "247"
+    },
+    {
+        "Entity": "United States",
+        "Code": "USA",
+        "Year": "1972",
+        "Terrorism incidents (GTD) (terrorism-related incidents)": "64"
+    },
+    {
+        "Entity": "United States",
+        "Code": "USA",
+        "Year": "1973",
+        "Terrorism incidents (GTD) (terrorism-related incidents)": "58"
+    },
+    {
+        "Entity": "United States",
+        "Code": "USA",
+        "Year": "1974",
+        "Terrorism incidents (GTD) (terrorism-related incidents)": "94"
+    },
+    {
+        "Entity": "United States",
+        "Code": "USA",
+        "Year": "1975",
+        "Terrorism incidents (GTD) (terrorism-related incidents)": "149"
+    },
+    {
+        "Entity": "United States",
+        "Code": "USA",
+        "Year": "1976",
+        "Terrorism incidents (GTD) (terrorism-related incidents)": "105"
+    },
+    {
+        "Entity": "United States",
+        "Code": "USA",
+        "Year": "1977",
+        "Terrorism incidents (GTD) (terrorism-related incidents)": "130"
+    },
+    {
+        "Entity": "United States",
+        "Code": "USA",
+        "Year": "1978",
+        "Terrorism incidents (GTD) (terrorism-related incidents)": "87"
+    },
+    {
+        "Entity": "United States",
+        "Code": "USA",
+        "Year": "1979",
+        "Terrorism incidents (GTD) (terrorism-related incidents)": "69"
+    },
+    {
+        "Entity": "United States",
+        "Code": "USA",
+        "Year": "1980",
+        "Terrorism incidents (GTD) (terrorism-related incidents)": "67"
+    },
+    {
+        "Entity": "United States",
+        "Code": "USA",
+        "Year": "1981",
+        "Terrorism incidents (GTD) (terrorism-related incidents)": "73"
+    },
+    {
+        "Entity": "United States",
+        "Code": "USA",
+        "Year": "1982",
+        "Terrorism incidents (GTD) (terrorism-related incidents)": "78"
+    },
+    {
+        "Entity": "United States",
+        "Code": "USA",
+        "Year": "1983",
+        "Terrorism incidents (GTD) (terrorism-related incidents)": "44"
+    },
+    {
+        "Entity": "United States",
+        "Code": "USA",
+        "Year": "1984",
+        "Terrorism incidents (GTD) (terrorism-related incidents)": "63"
+    },
+    {
+        "Entity": "United States",
+        "Code": "USA",
+        "Year": "1985",
+        "Terrorism incidents (GTD) (terrorism-related incidents)": "40"
+    },
+    {
+        "Entity": "United States",
+        "Code": "USA",
+        "Year": "1986",
+        "Terrorism incidents (GTD) (terrorism-related incidents)": "49"
+    },
+    {
+        "Entity": "United States",
+        "Code": "USA",
+        "Year": "1987",
+        "Terrorism incidents (GTD) (terrorism-related incidents)": "34"
+    },
+    {
+        "Entity": "United States",
+        "Code": "USA",
+        "Year": "1988",
+        "Terrorism incidents (GTD) (terrorism-related incidents)": "27"
+    },
+    {
+        "Entity": "United States",
+        "Code": "USA",
+        "Year": "1989",
+        "Terrorism incidents (GTD) (terrorism-related incidents)": "42"
+    },
+    {
+        "Entity": "United States",
+        "Code": "USA",
+        "Year": "1990",
+        "Terrorism incidents (GTD) (terrorism-related incidents)": "32"
+    },
+    {
+        "Entity": "United States",
+        "Code": "USA",
+        "Year": "1991",
+        "Terrorism incidents (GTD) (terrorism-related incidents)": "30"
+    },
+    {
+        "Entity": "United States",
+        "Code": "USA",
+        "Year": "1992",
+        "Terrorism incidents (GTD) (terrorism-related incidents)": "32"
+    },
+    {
+        "Entity": "United States",
+        "Code": "USA",
+        "Year": "1994",
+        "Terrorism incidents (GTD) (terrorism-related incidents)": "56"
+    },
+    {
+        "Entity": "United States",
+        "Code": "USA",
+        "Year": "1995",
+        "Terrorism incidents (GTD) (terrorism-related incidents)": "60"
+    },
+    {
+        "Entity": "United States",
+        "Code": "USA",
+        "Year": "1996",
+        "Terrorism incidents (GTD) (terrorism-related incidents)": "35"
+    },
+    {
+        "Entity": "United States",
+        "Code": "USA",
+        "Year": "1997",
+        "Terrorism incidents (GTD) (terrorism-related incidents)": "40"
+    },
+    {
+        "Entity": "United States",
+        "Code": "USA",
+        "Year": "1998",
+        "Terrorism incidents (GTD) (terrorism-related incidents)": "30"
+    },
+    {
+        "Entity": "United States",
+        "Code": "USA",
+        "Year": "1999",
+        "Terrorism incidents (GTD) (terrorism-related incidents)": "53"
+    },
+    {
+        "Entity": "United States",
+        "Code": "USA",
+        "Year": "2000",
+        "Terrorism incidents (GTD) (terrorism-related incidents)": "32"
+    },
+    {
+        "Entity": "United States",
+        "Code": "USA",
+        "Year": "2001",
+        "Terrorism incidents (GTD) (terrorism-related incidents)": "41"
+    },
+    {
+        "Entity": "United States",
+        "Code": "USA",
+        "Year": "2002",
+        "Terrorism incidents (GTD) (terrorism-related incidents)": "33"
+    },
+    {
+        "Entity": "United States",
+        "Code": "USA",
+        "Year": "2003",
+        "Terrorism incidents (GTD) (terrorism-related incidents)": "32"
+    },
+    {
+        "Entity": "United States",
+        "Code": "USA",
+        "Year": "2004",
+        "Terrorism incidents (GTD) (terrorism-related incidents)": "9"
+    },
+    {
+        "Entity": "United States",
+        "Code": "USA",
+        "Year": "2005",
+        "Terrorism incidents (GTD) (terrorism-related incidents)": "20"
+    },
+    {
+        "Entity": "United States",
+        "Code": "USA",
+        "Year": "2006",
+        "Terrorism incidents (GTD) (terrorism-related incidents)": "6"
+    },
+    {
+        "Entity": "United States",
+        "Code": "USA",
+        "Year": "2007",
+        "Terrorism incidents (GTD) (terrorism-related incidents)": "9"
+    },
+    {
+        "Entity": "United States",
+        "Code": "USA",
+        "Year": "2008",
+        "Terrorism incidents (GTD) (terrorism-related incidents)": "18"
+    },
+    {
+        "Entity": "United States",
+        "Code": "USA",
+        "Year": "2009",
+        "Terrorism incidents (GTD) (terrorism-related incidents)": "10"
+    },
+    {
+        "Entity": "United States",
+        "Code": "USA",
+        "Year": "2010",
+        "Terrorism incidents (GTD) (terrorism-related incidents)": "17"
+    },
+    {
+        "Entity": "United States",
+        "Code": "USA",
+        "Year": "2011",
+        "Terrorism incidents (GTD) (terrorism-related incidents)": "10"
+    },
+    {
+        "Entity": "United States",
+        "Code": "USA",
+        "Year": "2012",
+        "Terrorism incidents (GTD) (terrorism-related incidents)": "19"
+    },
+    {
+        "Entity": "United States",
+        "Code": "USA",
+        "Year": "2013",
+        "Terrorism incidents (GTD) (terrorism-related incidents)": "20"
+    },
+    {
+        "Entity": "United States",
+        "Code": "USA",
+        "Year": "2014",
+        "Terrorism incidents (GTD) (terrorism-related incidents)": "26"
+    },
+    {
+        "Entity": "United States",
+        "Code": "USA",
+        "Year": "2015",
+        "Terrorism incidents (GTD) (terrorism-related incidents)": "39"
+    },
+    {
+        "Entity": "United States",
+        "Code": "USA",
+        "Year": "2016",
+        "Terrorism incidents (GTD) (terrorism-related incidents)": "61"
+    }
+];
